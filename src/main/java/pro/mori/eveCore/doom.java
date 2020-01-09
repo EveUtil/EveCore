987d4f4be400b71c44ae0716594eb0a82f68b417
末日 <1302796097@qq.com>
2020-01-09 14:13:24 +0800

    ok
    v1.0

---
+++ src/main/java/pro/mori/eveCore/doom.java
@@ -0,0 +1,35 @@
+package pro.mori.eveCore;
+
+import pro.mori.moriUtil.Util.ClassUtil;
+import pro.mori.moriUtil.Util.FormatUtil;
+import pro.mori.moriUtil.Util.impl.classImpl;
+import pro.mori.moriUtil.Util.impl.formatImpl;
+import pro.mori.moriUtil.common.UrlEnum;
+
+
+public class doom {
+
+    private ClassUtil classUtil = new classImpl();
+
+    private FormatUtil formatUtil = new formatImpl();
+
+    private Class<?> cls;
+
+    private String[] args;
+
+    public doom(Class<?> cls,String[] args) {
+        this.cls = cls;
+        this.args = args;
+    }
+
+    public void run() {
+        System.out.println("执行成功");
+
+    }
+    public doom test(){
+        System.out.println(formatUtil.test());
+        System.out.println(classUtil.getClassPath(cls));
+        System.out.println(UrlEnum.EVE_PRICE_API.getMag());
+        return this;
+    }
+}
