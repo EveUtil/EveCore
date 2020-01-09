354b1b9c6e177b71311d71a84d9b7ae02c9c9647
末日 <1302796097@qq.com>
2020-01-09 20:51:02 +0800

    v1.0.2

--- src/main/java/pro/mori/eveCore/doom.java
+++ src/main/java/pro/mori/eveCore/doom.java
@@ -3,8 +3,10 @@ package pro.mori.eveCore;
 
 import pro.mori.moriUtil.Util.ClassUtil;
 import pro.mori.moriUtil.Util.FormatUtil;
+import pro.mori.moriUtil.Util.HttpClient;
 import pro.mori.moriUtil.Util.impl.classImpl;
 import pro.mori.moriUtil.Util.impl.formatImpl;
+import pro.mori.moriUtil.Util.impl.httpClientImpl;
 import pro.mori.moriUtil.common.UrlEnum;
 
 public class doom {
@@ -12,6 +14,8 @@ public class doom {
     private ClassUtil classUtil = new classImpl();
 
     private FormatUtil formatUtil = new formatImpl();
+
+    private HttpClient httpClient = new httpClientImpl();
 
     private Class<?> cls;
 
@@ -30,6 +34,8 @@ public class doom {
         System.out.println(formatUtil.test());
         System.out.println(classUtil.getClassPath(cls));
         System.out.println(UrlEnum.EVE_PRICE_API.getMag());
+        String[] s = {"1","2","3"};
+        httpClient.getParam(s);
         return this;
     }
 }
